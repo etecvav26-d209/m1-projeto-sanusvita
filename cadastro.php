566397486db1cef56b768661a55c61355fcd9a24
laura duarte <dduartelaureta@gmail.com>
2026-05-18 12:36:50 -0300

    adicionando para pegar os dados

--- cadastro.php
+++ cadastro.php
@@ -4,6 +4,12 @@ require_once 'config/conexao.php';
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+$nome = $_POST['nome'];
+    $fabricante = $_POST['fabricante'];
+    $preco = $_POST['preco'];
+    $estoque = $_POST['estoque'];
+
+   
 }
 
 ?>
